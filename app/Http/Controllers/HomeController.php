dd8e2c4d8f7258a1cd1ed76b50e89fca9084dc2b
inderpreet-narula <inderpreet.s.narula@gmail.com>
2020-09-28 17:48:42 +0530

    Created visitor page and its related db table/model

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Visitor;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $visitor = Visitor::first();        
+        $new_count = $visitor->visitor+1;
+        $visitor->visitor = $new_count;
+        $visitor->save();
+        return view('home', compact('new_count'));
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+
+<div style="display: flex;
+    flex-direction: column;
+    align-items: center;">
+    <div ><h3>Welcome!!</h3></div>
+    <div>You are visitor number {{$new_count}}</div>
+<div>
