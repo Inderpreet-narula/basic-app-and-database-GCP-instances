36908c593cd79b7941f533e9830d0b2562d720c9
inderpreet-narula <inderpreet.s.narula@gmail.com>
2020-10-01 16:22:57 +0530

    Added dynamic user count code in homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,11 +9,10 @@ class HomeController extends Controller
 {
     public function index()
     {
-        // $visitor = Visitor::first();        
-        // $new_count = $visitor->visitor+1;
-        // $visitor->visitor = $new_count;
-        // $visitor->save();
-        // return view('home', compact('new_count'));
-        return view('welcome');
+        $visitor = Visitor::first();        
+        $new_count = $visitor->visitor+1;
+        $visitor->visitor = $new_count;
+        $visitor->save();
+        return view('home', compact('new_count'));
     }
 }
